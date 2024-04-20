e4b26a57aa11373c37a9bbea9fff1f41262bfb1d
DINESH-2611 <159759453+DINESH-2611@users.noreply.github.com>
2024-04-20 14:17:07 +0530

    Add files via upload

---
+++ SudukoSolver.java
@@ -0,0 +1,115 @@
+package sudokosolver;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SudukoSolver {
+    Scanner scanner=new Scanner(System.in);
+    private void createSudoko(){
+        int[][] sudoko={{5,3,4,0,7,8,9,1,2},
+                        {6,0,2,1,9,5,3,4,8},
+                        {0,9,8,3,4,2,5,6,7},
+                        {8,5,0,7,6,1,4,2,3},
+                        {4,2,6,8,0,3,7,9,1},
+                        {7,1,3,9,2,0,8,5,6},
+                        {9,6,1,5,3,7,0,8,4},
+                        {2,8,7,4,1,9,6,3,5},
+                        {3,4,5,2,8,6,1,7,0}};
+        printSudoko(sudoko);
+        int empty=0;
+        int count=0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(sudoko[i][j]!=0){
+                    count++;
+                }
+            }
+        }
+        empty=81-count;
+        getVal(sudoko,empty);
+    }
+    private boolean validRow(int row,int val,int[][] sudoko){
+        Set<Integer> set =new HashSet<>();
+        for(int i=0;i<9;i++){
+            set.add(sudoko[row][i]);
+        }
+        return set.add(val);
+    }
+    private boolean validCol(int col,int val,int[][] sudoko){
+        Set<Integer> set =new HashSet<>();
+        for(int i=0;i<9;i++){
+            set.add(sudoko[i][col]);
+        }
+        return set.add(val);
+
+    }
+    private boolean validSubMatrix(int row,int col,int val,int[][] sudoko){
+        Set<Integer> set =new HashSet<>();
+        int subRow=row-row%3;
+        int subCol=col-col%3;
+        for (int i = subRow; i <subRow+3 ; i++) {
+            for (int j = subCol; j <subCol+3 ; j++) {
+                set.add(sudoko[i][j]);
+            }
+        }
+        return set.add(val);
+    }
+    public void getVal(int[][] suduko,int count){
+        int wrong=0,repeat=0;
+        for (int k = 0; k < count; k++) {
+            System.out.println("Enter the index in 'i j' format");
+            int i=scanner.nextInt(),j=scanner.nextInt();
+            if(suduko[i][j]!=0){
+                if(repeat>3){
+                    System.out.println("You have choose already filled index 5 times and lost this game");
+                    return;
+                }
+                System.out.println("Index already filled,Choose another index");
+                k--;
+                repeat++;
+            }else {
+                System.out.println("Enter the value");
+                int val=scanner.nextInt();
+                if (validRow(i,val,suduko) && validCol(j,val,suduko) && validSubMatrix(i, j,val,suduko)) {
+                    suduko[i][j]=val;
+                }else{
+                    if(wrong>1){
+                        System.out.println("You made 3 mistakes and lost this game");
+                        return;
+                    }
+                    suduko[i][j]=val;
+                    wrong++;
+                }
+            }
+        }
+        if(isValidSudoku(suduko))
+            System.out.println("Congratulation!...You have solved the Suduko");
+        else
+            System.out.println("You lost this game");
+    }
+    private void printSudoko(int[][] sudoko){
+        for(int[] sub:sudoko){
+            for(int val:sub){
+                System.out.print(val+" ");
+            }
+            System.out.println();
+        }
+    }
+    private boolean isValidSudoku(int[][] sudoku){
+        Set<String> set=new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j <9 ; j++) {
+                int val=sudoku[i][j];
+                if(!set.add(val+"in row"+i) || !set.add(val+"in column"+j) || !set.add(val+"in block"+i/3+"-"+j/3))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        new SudukoSolver().createSudoko();
+    }
+}
+
